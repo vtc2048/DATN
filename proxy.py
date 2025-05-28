91a204955fd306bf6c2b92712df4ad2ce3ba5493
vtc2048 <congvantan90@gmail.com>
2025-05-29 05:17:16 +0700

    fix: use dynamic port for Render

--- proxy.py
+++ proxy.py
@@ -23,5 +23,5 @@ def get_data():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port) 
 
