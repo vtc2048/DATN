48e3719f620f2dfe5b2a76635afcc02d324ee25c
vtc2048 <congvantan90@gmail.com>
2025-05-29 07:07:02 +0700

    debug: print start message

--- proxy.py
+++ proxy.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello from Flask on Render!"
+@app.route('/api/data')
+def test_data():
+    return jsonify({
+        "object": {
+            "temperature": 30.5,
+            "humidity": 70,
+            "latitude": 16.05,
+            "longitude": 108.2,
+            "pm25": 40,
+            "pm10": 50,
+            "no2": 0.02,
+            "so2": 0.01,
+            "co": 0.05,
+            "uv": 5.5
+        }
+    })
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
