938ee125480ed448edab76f15f043651d86fa5b1
vtc2048 <congvantan90@gmail.com>
2025-05-29 07:02:27 +0700

    debug: print start message

--- proxy.py
+++ proxy.py
@@ -1,29 +1,12 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
-import requests
+from flask import Flask
 import os
 
-print("✅ proxy.py is running...")
 app = Flask(__name__)
-CORS(app)  # Cho phép CORS
 
-API_URL = 'http://vngalaxy.vn:5000/get_data'
-TOKEN = '43497e17-9d24-4b08-97f1-4a08366bb9f9'
-
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    try:
-        headers = {
-            'Authorization': f'Bearer {TOKEN}'
-        }
-        response = requests.get(API_URL, headers=headers)
-        response.raise_for_status()
-        return jsonify(response.json())
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+@app.route('/')
+def hello():
+    return "Hello from Flask on Render!"
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
- 
-
