b0bf877fcfc135b5b3e9f5e9273b95c84f977597
vtc2048 <81420976+vtc2048@users.noreply.github.com>
2025-06-02 23:10:27 +0700

    Update script.js
    Signed-off-by: vtc2048 <81420976+vtc2048@users.noreply.github.com>

--- script.js
+++ script.js
@@ -119,7 +119,7 @@ function loadSavedAQI() {
                     stroke: false,
                     fillColor: color,
                     fillOpacity: 0.6,
-                    radius: 30
+                    radius: 10
                 }).addTo(map).bindPopup(`AQI: ${item.aqi} (${item.level})`);
                 aqiCircles.push(circle);
             });
@@ -155,7 +155,7 @@ function fetchData() {
                     stroke: false,
                     fillColor: aqiColor,
                     fillOpacity: 0.6,
-                    radius: 30
+                    radius: 10
                 }).addTo(map).bindPopup(`AQI: ${aqiData.aqi} (${aqiData.level})`);
                 aqiCircles.push(circle);
             }
